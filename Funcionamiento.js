ed390037c75f7e96fdbab170add11054143227ab
LatiSound Music <latisoundmusic@gmail.com>
2025-06-26 19:32:06 -0500

    Primera subida del sitio web

---
+++ Funcionamiento.js
@@ -0,0 +1,53 @@
+let slideIndex = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = slides.children.length;
+
+// Función para mover los slides
+function moveSlide(direction) {
+    slideIndex += direction;
+
+    // Si se pasa al anterior del primero
+    if (slideIndex < 0) {
+        slideIndex = totalSlides - 1;
+    }
+
+    // Si se pasa del último
+    if (slideIndex >= totalSlides) {
+        slideIndex = 0;
+    }
+
+    updateSlidePosition();
+}
+
+// Actualiza la posición del slide
+function updateSlidePosition() {
+    slides.style.transform = `translateX(-${slideIndex * 100}%)`;
+}
+
+// Movimiento automático cada 5 segundos
+setInterval(() => {
+    moveSlide(1);
+}, 10000);
+
+// Permite que las funciones estén disponibles para HTML
+window.moveSlide = moveSlide;
+
+
+function scrollReel(direction) {
+    const reel = document.querySelector('.reel-container');
+    const scrollAmount = 320;
+    const maxScroll = reel.scrollWidth - reel.clientWidth;
+    
+    let newScrollLeft = reel.scrollLeft + direction * scrollAmount;
+    
+    if (newScrollLeft < 0) {
+        newScrollLeft = 0;
+    } else if (newScrollLeft > maxScroll) {
+        newScrollLeft = maxScroll;
+    }
+    
+    reel.scrollTo({
+        left: newScrollLeft,
+        behavior: 'smooth'
+    });
+}
